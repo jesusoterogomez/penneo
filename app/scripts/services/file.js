d94ddb1b0cd320fae439daaa3c2f29704af2b8ee
jesusoterogomez@gmail.com <jesusoterogomez@gmail.com>
2015-04-25 12:35:19 -0430

    Created file service

---
+++ app/scripts/services/file.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name penneoApp.file
+ * @description
+ * # file
+ * Service in the penneoApp.
+ */
+angular.module('penneoApp')
+  .service('fileService', function () {
+  	
+  	return ({
+  		list:list
+  	});
+
+  	function list () {
+  		
+  	}
+
+  });
+
